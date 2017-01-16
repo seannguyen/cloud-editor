5a4e95225425753a877f4ec312d31ad281258c65
seannguyen <sean.nguyentrong@gmail.com>
2017-01-17 03:00:25 +0800

    Added google drive verificarion middleware

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,32 +1,35 @@
+require 'google/apis/drive_v3'
+require 'google/api_client/client_secrets'
+
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
-
+  before_action :verify_google_drive_auth
+  
   # GET /notes
   # GET /notes.json
   def index
     @notes = Note.all
   end
-
+  
   # GET /notes/1
   # GET /notes/1.json
   def show
-    
   end
-
+  
   # GET /notes/new
   def new
     @note = Note.new
   end
-
+  
   # GET /notes/1/edit
   def edit
   end
-
+  
   # POST /notes
   # POST /notes.json
   def create
     @note = Note.new(note_params)
-
+    
     respond_to do |format|
       if @note.save
         format.html { redirect_to @note, notice: 'Note was successfully created.' }
@@ -37,7 +40,7 @@ class NotesController < ApplicationController
       end
     end
   end
-
+  
   # PATCH/PUT /notes/1
   # PATCH/PUT /notes/1.json
   def update
@@ -51,7 +54,7 @@ class NotesController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
@@ -61,15 +64,39 @@ class NotesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_note
-      @note = Note.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_note
+    @note = Note.find(params[:id])
+  end
+  
+  def verify_google_drive_auth
+    # Check credential ofr gg drive has been in place yet
+    return session[:google_drive_credential] if session[:google_drive_credential]
+    
+    # If not go ahead and get one
+    client_secrets = Google::APIClient::ClientSecrets.load 'config/google_api_client_secret.json'
+    auth_client = client_secrets.to_authorization
+    auth_client.update!(
+        :scope => 'https://www.googleapis.com/auth/drive.metadata.readonly',
+        :redirect_uri => request.original_url
+    )
+    
+    if request[:code] == nil
+      auth_uri = auth_client.authorization_uri.to_s
+      redirect_to auth_uri
+    else
+      auth_client.code = request['code']
+      auth_client.fetch_access_token!
+      auth_client.client_secret = nil
+      session[:google_drive_credential] = auth_client.to_json
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def note_params
-      params.fetch(:note, {})
-    end
+  end
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def note_params
+    params.fetch(:note, {})
+  end
 end
